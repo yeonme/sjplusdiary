3b04826241505c7374b8f26a627713ecedc133c1
Jinbaek Lee <yeon@yeon.me>
2018-06-25 10:55:26 +0900

    index: calendar+style+title

--- item.php
+++ item.php
@@ -1,24 +1,37 @@
+<?php
+
+?>
+<div class="title">
+  <h1 id="readonlyTitle">타이포그래피에 대한 고민.</h1>
+  <textarea id="titleEdit" rows='1' data-min-rows='1' placeholder='Title' class="autoExpand"></textarea>
+</div>
+<div id="readonlynote">
+
+</div>
+<div id="writablenote">
+<div id="summernote" style="height:100%;">Hello Summernote</div><?php /*
 <div class="ui grid" style="min-height:100%;">
-  <div class="twelve wide stretched column" style="height:100%;">
-    <div class="ui segment" style="height:100%;">
-        <div id="summernote" style="height:100%;">Hello Summernote</div>
-    </div>
-  </div>
-  <div class="four wide column">
-    <div class="ui vertical fluid right tabular menu">
-      <a class="item">
-        Bio
-      </a>
-      <a class="item">
-        Pics
-      </a>
-      <a class="item active">
-        Companies
-      </a>
-      <a class="item">
-        Links
-      </a>
-    </div>
-  </div>
+<div class="twelve wide stretched column" style="height:100%;">
+<div class="ui segment" style="height:100%;">
+
+</div>
+</div>
+<div class="four wide column">
+<div class="ui vertical fluid right tabular menu">
+<a class="item">
+Bio
+</a>
+<a class="item">
+Pics
+</a>
+<a class="item active">
+Companies
+</a>
+<a class="item">
+Links
+</a>
+</div>
+</div>
+</div>*/?>
 </div>
 <script>cloadFinish();</script>
